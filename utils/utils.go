3516f6d579da9b46b035c5dd236fdd8a215a7154
Rahul Somasundaram <rahul.somasundaram@checkpt.com>
2022-03-31 14:08:55 +0530

    added IsObjectEmpty

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"reflect"
 
 	"gopkg.in/yaml.v2"
 )
@@ -61,4 +62,25 @@ func DeleteFiles(files ...string) {
 	for _, fp := range files {
 		DeleteFile(fp)
 	}
+}
+
+func IsObjectEmpty(object interface{}) bool {
+	//First check normal definitions of empty
+	if object == nil {
+		return true
+	} else if object == "" {
+		return true
+	} else if object == false {
+		return true
+	}
+
+	//Then see if it's a struct
+	if reflect.ValueOf(object).Kind() == reflect.Struct {
+		// and create an empty copy of the struct object to compare against
+		empty := reflect.New(reflect.TypeOf(object)).Elem().Interface()
+		if reflect.DeepEqual(object, empty) {
+			return true
+		}
+	}
+	return false
 }
